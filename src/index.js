926b258e9c706cff75aa52aed594a4f9d1f43fa1
Radoslav Demirov <rdemirov@gmail.com>
2017-08-31 22:09:07 +0300

    fix : Pass store as a property to the app component

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
