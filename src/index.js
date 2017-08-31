2b2ef8421cea5bb7665a50d97a0718c3ab32c139
Radoslav Demirov <rdemirov@gmail.com>
2017-08-31 21:53:12 +0300

    feat : Create Redux Store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import {createStore} from 'redux';
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
